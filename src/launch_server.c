bd1f9d28bd9c9bd3c850ebe4cd91811a10dffe06
Enerdhil <hezardj@gmail.com>
2017-04-28 17:53:08 +0200

    Add(server): Now effectively listening and doing nothing

--- src/launch_server.c
+++ src/launch_server.c
@@ -22,19 +22,37 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <args.h>
 
 #define BACKLOG 10 /* How many pending connection we will keep in queue */
 
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
 void launch_server(void) {
-    int             sockfd = 0, /* Read on this one */
-                    rval;       /* To stock getaddrinfo() return */
-    s8_t            enable = 1; /* Int to enable the option in setsockopt */
-    struct addrinfo hints,      /* Flags of getaddrinfo() */
-                    *servinfo,  /* Result of getaddrinfo() */
+    int             sockfd = 0,  /* Read on this one */
+                    rem_fd = 0,  /* Fd that will be attributed to the client */
+                    rval;        /* To stock getaddrinfo() return */
+    s8_t            enable = 1;  /* Int to enable the option in setsockopt */
+    struct addrinfo hints,       /* Flags of getaddrinfo() */
+                    *servinfo,   /* Result of getaddrinfo() */
                     *ptr;        /* Variable used to loop into the linked lst*/
     char            port_str[6]; /* Buffer to hold the port number */
+
+    struct sockaddr_storage rem_addr;     /* Hold client infos */
+    socklen_t               sin_size = 0; /* Size of rem_addr */
+
+    char                    str[INET6_ADDRSTRLEN];
 
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;     /* Don't care IPv4 and IPv6 */
@@ -57,7 +75,7 @@ void launch_server(void) {
                 ptr->ai_socktype, ptr->ai_protocol)) == -1)
         {
             /* If it fails, display the error and try with the next struct */
-            m_error("Server: socket" , strerror(errno));
+            m_error("Server: socket %s" , strerror(errno));
             continue ;
         }
         /* If socket succeded, call setsockopt */
@@ -65,14 +83,14 @@ void launch_server(void) {
             sizeof(int)) == 1)
         {
             /* If it fails, display the error and exit */
-            m_panic("Server: socket" , strerror(errno));
+            m_panic("Server: socket %s" , strerror(errno));
         }
         /* If we got here, try to bind to the socket */
         if (bind(sockfd, ptr->ai_addr, ptr->ai_addrlen) == -1)
         {
             /* If it fails, display the error and try with the next struct */
             close(sockfd);
-            m_error("Server: bind" , strerror(errno));
+            m_error("Server: bind %s" , strerror(errno));
             continue ;
         }
         /* If all that succeded, get out of the loop */
@@ -89,9 +107,26 @@ void launch_server(void) {
 
     if (listen(sockfd, BACKLOG) == -1)
     {
-        m_panic("Server: listen" , strerror(errno));
+        /* Exit if listen() fails */
+        m_panic("Server: listen %s" , strerror(errno));
     }
 
-    m_info("Waiting for connections ...");
+    m_info("Waiting for connections ...\n");
 
+    /* This is the main accept() loop */
+    while (1)
+    {
+        sin_size = sizeof(rem_addr);
+        /* Try to get a fd for the client */
+        if ((rem_fd = accept(sockfd, (struct sockaddr *)&rem_addr, 
+             &sin_size)) == -1)
+        {
+            /* If it fails, try again */
+            m_error("Server: accept %s", strerror);
+            continue ;
+        }
+        inet_ntop(rem_addr.ss_family, get_in_addr((struct sockaddr *)&rem_addr),
+                    str, sizeof str);
+        m_info("Server: Got connection from %s\n", str);
+    }
 }
